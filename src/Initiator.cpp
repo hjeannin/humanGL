c661908b034bf127a5939892f98ee3f434b72ef5
Hugo JEANNIN <hjeannin@e1r7p10.42.fr>
2016-01-19 19:17:48 +0100

    ahhhhhhhhhhhhhhhhhhh putain de vertices_size

--- src/Initiator.cpp
+++ src/Initiator.cpp
@@ -144,6 +144,8 @@ Initiator::generateRandomModel(void)
 void
 Initiator::generateSphere(void)
 {
+	int				number_of_lines = 8;
+	GLfloat			space = 0.1;
 	int				index = 0;
 	int				index2 = 0;
 	double			pi = M_PI;
@@ -153,9 +155,8 @@ Initiator::generateSphere(void)
 	GLubyte			color_r = 0;
 	GLubyte			color_g = 123;
 	GLubyte			color_b = 255;
-	int				number_of_lines = 8;
 	int				number_of_points = number_of_lines * number_of_lines;
-	int				number_of_faces = number_of_points * 2 - 16;
+	int				number_of_faces = (number_of_points * 2) - (number_of_lines * 2);
 	this->vertices_num_elem = number_of_points;
 	this->faces_num_elem = 3 * number_of_faces;
 	this->vertices_array = new Point[this->vertices_num_elem];
@@ -178,6 +179,7 @@ Initiator::generateSphere(void)
 			// std::cout << vertices_array[i].x << "," << vertices_array[i].y << "," << vertices_array[i].z << std::endl;
 			index++;
 		}
+		space += 0.5;
 	}
 	for (int j = 0; j < faces_num_elem; j += 6)
 	{
@@ -190,7 +192,7 @@ Initiator::generateSphere(void)
 		index2++;
 	}
 	printArray(faces_array, faces_num_elem);
-	// printPointArray(this->vertices_array, this->vertices_num_elem);
+	printPointArray(this->vertices_array, this->vertices_num_elem);
 }
 
 void
@@ -202,7 +204,7 @@ Initiator::createImage(void)
 	glBindVertexArray(this->vao);
 	glGenBuffers(2, &this->vbos[0]);
 
-	this->vertices_size = sizeof(GLfloat) * this->vertices_num_elem;
+	this->vertices_size = sizeof(GLfloat) * this->vertices_num_elem * 4;
 	glBindBuffer(GL_ARRAY_BUFFER, this->vbos[0]);
 	glBufferData(GL_ARRAY_BUFFER, this->vertices_size, this->vertices_array,
 					GL_STATIC_DRAW);
